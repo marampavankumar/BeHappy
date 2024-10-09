d3afce3b4461f7d2413bf96d25bb0645dbc3c0d6
marampavankumar <marampavankumar@gmail.com>
2024-10-09 15:33:58 +0530

    Add files via upload

--- Test/src/test/TestBuildOrder.java
+++ Test/src/test/TestBuildOrder.java
@@ -1,4 +1,4 @@
-package test;
+package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,163 +6,166 @@ import java.util.List;
 import java.util.Stack;
 
 public class TestBuildOrder {
-	
-	ArrayList<Node> nodes = new ArrayList<>();
 
-	public static void main(String[] args) {
-		TestBuildOrder buildOrder = new TestBuildOrder();
-		buildOrder.buildTree("M1", Arrays.asList("M2", "M3", "M4"));
-		buildOrder.buildTree("M2", Arrays.asList("M11", "M12", "M8"));
-		buildOrder.buildTree("M3", Arrays.asList("M7", "M8", "M2"));
-		buildOrder.buildTree("M4", Arrays.asList("M5", "M6", "M3"));
-		buildOrder.buildTree("M12", Arrays.asList("M8"));
+  ArrayList<Node> nodes = new ArrayList<>();
 
-		buildOrder.printSequenceOrder();
+  public static void main(String[] args) {
+    TestBuildOrder buildOrder = new TestBuildOrder();
+    buildOrder.buildTree("fiserver", Arrays.asList("common", "logging", "utils", "security", "platform", "external"));
+    buildOrder.buildTree("isagent", Arrays.asList("common", "logging", "utils", "jpos"));
+    buildOrder.buildTree("logging", List.of("common"));
+    buildOrder.buildTree("utils", Arrays.asList("common", "external"));
+    buildOrder.buildTree("security", List.of("common"));
+    buildOrder.buildTree("mgateway", Arrays.asList("common", "external", "platform", "utils", "logging", "security"));
+    buildOrder.buildTree("common", List.of("external"));
+    buildOrder.buildTree("setup", Arrays.asList("fiserver", "isagent", "mgateway"));
+    buildOrder.buildTree("tools", List.of("common"));
 
-	}
 
-	
+
+    buildOrder.printSequenceOrder();
+
+  }
+
+
   class Node
   {
-	  private String value;
-	  public String getValue() {
-		return value;
-	}
-	public void setValue(String value) {
-		this.value = value;
-	}
-	public ArrayList<Node> getChildren() {
-		return children;
-	}
-	public void setChildren(ArrayList<Node> children) {
-		this.children = children;
-	}
-	private ArrayList<Node> children = new ArrayList<>();
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + getEnclosingInstance().hashCode();
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Node other = (Node) obj;
-		if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
-	private TestBuildOrder getEnclosingInstance() {
-		return TestBuildOrder.this;
-	}
-	
-	
-  }
-  
-  void buildTree(String nodeValue, List<String> childrenValues)
-  {
-	  Node node=  new Node();
-	  node.setValue(nodeValue);
-	  if(nodes.contains(node)) 
-	  {
-		node = nodes.get(nodes.indexOf(node));  
-	  }
-	  else {
-		  nodes.add(node);
-	  }
-	  ArrayList<Node> childList  = new ArrayList<>();
-	  if(childrenValues != null && !childrenValues.isEmpty())
-	  {
-		  for(String childVal: childrenValues)
-		  {
-			  Node childNode=  new Node();
-			  childNode.setValue(childVal);
-			  if(nodes.contains(childNode)) 
-			  {
-				  childNode = nodes.get(nodes.indexOf(childNode));  
-			  }
-			  else {
-				  nodes.add(childNode);
-			  }
-			  
-			  childList.add(childNode);
-		  }
-	  }
-	  node.setChildren(childList);
-  }
-  
-  void printSequenceOrder()
-  {
-	  Node root = getRoot();
-	  System.out.println(root.getValue());
-	  ArrayList<Node> nodesVisitedForChildren = new ArrayList<>();
-	  ArrayList<Node> nodesVisited = new ArrayList<>();
-	  Stack<Node> stack = new Stack<>();
-	  stack.add(root);
-	  while(!stack.isEmpty())
-	  {
-		  Node node= stack.pop();
-		  if(nodesVisited.contains(node))
-		  {
-			  continue;
-		  }
-		  if(nodesVisitedForChildren.contains(node))
-		  {
-			  System.out.println("sequence::" + node.getValue());
-			  nodesVisited.add(node);
-			  continue;
-		  }
-		  if(node.getChildren() != null && !node.getChildren().isEmpty())
-		  {
-			  stack.push(node);
-			  for(Node child : node.getChildren())
-			  {
-				stack.push(child);  
-			  }
-			  nodesVisitedForChildren.add(node);
-		  }
-		  else 
-		  {
-			  System.out.println("sequence::" + node.getValue());
-			  nodesVisited.add(node);			  
-		  }
-	  }
+    private String value;
+    public String getValue() {
+      return value;
+    }
+    public void setValue(String value) {
+      this.value = value;
+    }
+    public List<Node> getParentModules() {
+      return parentModules;
+    }
+    public void setParentModules(List<Node> parentModules) {
+      this.parentModules = parentModules;
+    }
+    private List<Node> parentModules = new ArrayList<>();
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + getEnclosingInstance().hashCode();
+      result = prime * result + ((value == null) ? 0 : value.hashCode());
+      return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Node other = (Node) obj;
+      if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+        return false;
+      if (value == null) {
+        return other.value == null;
+      } else return value.equals(other.value);
+    }
+    private TestBuildOrder getEnclosingInstance() {
+      return TestBuildOrder.this;
+    }
+
+
   }
 
-private Node getRoot() 
-{
-	ArrayList<Node> childNodes = new ArrayList<>();
-	for(Node node: nodes)
-	{
-		if(node.getChildren() != null && !node.getChildren().isEmpty())
-		{
-			for(Node childNode: node.getChildren())
-			{
-				if(!childNodes.contains(childNode))
-				{
-					childNodes.add(childNode);
-				}
-			}
-		}
-	}
-	for(Node node: nodes)
-	{
-		if(!childNodes.contains(node))
-			return node;
-	}
-	return null;
-}
-	
+  void buildTree(String nodeValue, List<String> parentValues)
+  {
+    Node node=  new Node();
+    node.setValue(nodeValue);
+    if(nodes.contains(node))
+    {
+      node = nodes.get(nodes.indexOf(node));
+    }
+    else {
+      nodes.add(node);
+    }
+    ArrayList<Node> parentList  = new ArrayList<>();
+    if(parentValues != null && !parentValues.isEmpty())
+    {
+      for(String parentVal: parentValues)
+      {
+        Node parentNodeNode=  new Node();
+        parentNodeNode.setValue(parentVal);
+        if(nodes.contains(parentNodeNode))
+        {
+          parentNodeNode = nodes.get(nodes.indexOf(parentNodeNode));
+        }
+        else {
+          nodes.add(parentNodeNode);
+        }
+
+        parentList.add(parentNodeNode);
+      }
+    }
+    node.setParentModules(parentList);
+  }
+
+  void printSequenceOrder()
+  {
+    List<Node> leafNodes = getLeafNodes();
+    ArrayList<Node> nodesVisitedForParents = new ArrayList<>();
+    ArrayList<Node> nodesVisited = new ArrayList<>();
+    Stack<Node> stack = new Stack<>();
+    stack.addAll(leafNodes);
+    while(!stack.isEmpty())
+    {
+      Node node= stack.pop();
+      if(nodesVisited.contains(node))
+      {
+        continue;
+      }
+      if(nodesVisitedForParents.contains(node))
+      {
+        System.out.println("sequence::" + node.getValue());
+        nodesVisited.add(node);
+        continue;
+      }
+      if(node.getParentModules() != null && !node.getParentModules().isEmpty())
+      {
+        stack.push(node);
+        for(Node parent : node.getParentModules())
+        {
+          stack.push(parent);
+        }
+        nodesVisitedForParents.add(node);
+      }
+      else
+      {
+        System.out.println("sequence::" + node.getValue());
+        nodesVisited.add(node);
+      }
+    }
+  }
+
+  private List<Node> getLeafNodes()
+  {
+    List<Node> leafNodes = new ArrayList<>();
+    ArrayList<Node> parentNodes = new ArrayList<>();
+    for(Node node: nodes)
+    {
+      if(node.getParentModules() != null && !node.getParentModules().isEmpty())
+      {
+        for(Node parentNode: node.getParentModules())
+        {
+          if(!parentNodes.contains(parentNode))
+          {
+            parentNodes.add(parentNode);
+          }
+        }
+      }
+    }
+    for(Node node: nodes)
+    {
+      if(!parentNodes.contains(node))
+        leafNodes.add(node);
+    }
+    return leafNodes;
+  }
+
 }
